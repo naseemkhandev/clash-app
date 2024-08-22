f8afa9e4252574821c0a0a9fac4198918c92660f
Naseem Khan <devnaseemkhan@gmail.com>
2024-08-22 17:23:18 +0500

    feat: Add nodemailer for sending emails

---
+++ server/src/config/emali.ts
@@ -0,0 +1,27 @@
+import nodemailer from "nodemailer";
+
+export const transporter = nodemailer.createTransport({
+  host: process.env.SMTP_HOST,
+  port: parseInt(process.env.SMTP_PORT),
+  secure: false,
+  auth: {
+    user: process.env.SMTP_USER,
+    pass: process.env.SMTP_PASS,
+  },
+});
+
+export const sendMail = async (to: string, subject: string, body: string) => {
+  try {
+    const info = await transporter.sendMail({
+      from: process.env.EMAIL_FROM,
+      to,
+      subject,
+      html: body,
+    });
+
+    console.log("Message sent: %s", info.messageId);
+    console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+  } catch (error) {
+    console.error(error);
+  }
+};
